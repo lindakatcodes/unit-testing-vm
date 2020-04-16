a5f8e0fd21f8c29c02812568b75071e70fc11a42
Lindakat <lindakt16@gmail.com>
2020-04-15 19:13:11 -0500

    Init commit

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: true,
+  endOfLine: 'crlf'
+};
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <!-- Best practice in case you've got multiple inputs, since using class or id could change:
+    <input data-testid="name-input" type="text" v-model="name"/>
+    -->
+    <input type="text" v-model="name" />
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: ''
+      };
+    },
+    methods: {
+      onSubmit() {
+        this.$emit('formSubmitted', { name: this.name });
+      }
+    }
+  };
+</script>
